56b432a5ddc41d7b6c8ae8d4b5e580501a63b533
michalkerley3 <michael.kerley.3@us.af.mil>
2024-11-13 12:40:59 -0600

    working

--- Ethan/build.sh
+++ Ethan/build.sh
@@ -1,3 +1,3 @@
 
-export LD_LIBRARY_PATH=/home/vboxuser/projects/tangrampro-verify-examples/Ethan/code-gen/build/libs:/home/vboxuser/projects/tangrampro-verify-examples/Ethan/code-gen/local_install/lib
+export LD_LIBRARY_PATH=/home/vboxuser/projects/tangrampro-verify-examples/Ethan/code-gen/build/libs:/home/vboxuser/projects/tangrampro-verify-examples/Ethan/code-gen/local_install/lib:/home/vboxuser/projects/tangLinuxTest/Michael/code-gen/build/libs:/home/vboxuser/projects/tangLinuxTest/Michael/code-gen/local_install/lib
 make
